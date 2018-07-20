c660121d7ecda89058e28119ac3c5a737e94e05d
root <guilhermebehs2013@hotmail.com>
2018-07-19 21:56:02 -0300

    Criação do server.js

---
+++ NODE JS/Cadastro de Cliente/config/server.js
@@ -0,0 +1,20 @@
+module.exports = function(){
+
+   var express = require('express');
+   var bodyParser = require('body-parser');
+   var expressValidator = require('express-validator');
+   var aplicacao = express();
+   
+   aplicacao = express();
+   aplicacao.set('view engine', 'ejs');
+   aplicacao.set('views, ./view');
+   aplicacao.use(bodyParser.json());
+   aplicacao.use(bodyParser.urlencoded({extended:true}));
+   aplicacao.use(expressValidator());
+   
+   var rotas = require('../routes/web');
+   rotas(aplicacao);
+   aplicacao.listen(8000, console.log('Escutando na porta 8000'));
+    
+
+};

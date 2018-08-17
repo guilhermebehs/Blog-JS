3ce6f4486a57e2eff28927f5b565ac576d95926a
guilhermebehs <38084311+guilhermebehs@users.noreply.github.com>
2018-08-17 16:48:52 -0300

    Create postagemController.js

---
+++ NODE JS/Cadastro de Cliente/controller/postagemController.js
@@ -0,0 +1,27 @@
+var base = require('../model/postagemModel');
+
+
+
+module.exports.retornarTodos = function(req,res){
+
+   base.getAll(function(erro, resultado){
+       if(erro)
+          console.log("Ocorreu um erro");
+       else
+       	  res.render('home', {dados:resultado, erros:{}});
+
+   });
+
+}; 
+
+
+module.exports.criar = function(req,res){
+   var dados = req.body;
+   console.log(dados);
+   base.save(dados,function(erro, resultado){
+   	  if(erro)
+   	  	 console.log(erro);
+      res.render('resposta');
+   });
+   
+};
